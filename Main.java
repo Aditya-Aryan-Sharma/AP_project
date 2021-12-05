0608b8058521bb94ced9f094b0964f3f52f95596
Tushar0805 <88985334+Tushar0805@users.noreply.github.com>
2021-12-05 13:58:54 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+package com.example.game;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException{
+
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainWindow.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            stage.setScene(scene);
+            stage.setTitle("Will Hero");
+
+            Image image = new Image("D:\\SEM_3\\WillHero\\Icon.png");
+            stage.getIcons().setAll(image);
+            stage.setResizable(false);
+            stage.show();
+
+            stage.setOnCloseRequest(event -> {
+                event.consume();
+                exit(stage);
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void exit(Stage stage){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Quit game");
+        alert.setContentText("Do you want to save before exiting?");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+//            stage = (Stage) scenePane.getScene().getWindow();
+//            System.out.println("You successfully logged out!");
+            System.out.println("Game closed.");
+            stage.close();
+        }
+    }
+}
