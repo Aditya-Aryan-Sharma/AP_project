600ab0751c0692a915e672a1608022b522784ef3
Aditya Aryan Sharma <83038560+Aditya-Aryan-Sharma@users.noreply.github.com>
2021-12-18 13:39:02 +0530

    Game Graphics

--- src/PlayGameWindowController.java
+++ src/PlayGameWindowController.java
@@ -1,19 +1,19 @@
 package com.example.game;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
+import javafx.beans.property.DoubleProperty;
 import javafx.css.converter.FontConverter;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBox;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -24,41 +24,28 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Timer;
 
 public class PlayGameWindowController implements Initializable {
-
+    private int position;
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private User user;
 
-    public void up(ActionEvent event){
-
+    private void Animate(Node myNode,Duration duration,int type,double value){
+        TranslateTransition translateTransition = new TranslateTransition();
+        translateTransition.setNode(myNode);
+        translateTransition.setDuration(duration);
+        if (type==1)
+            translateTransition.setCycleCount(TranslateTransition.INDEFINITE);
+        translateTransition.setByY(value);
+        translateTransition.setAutoReverse(true);
+        translateTransition.play();
     }
-
-    public void down(ActionEvent event){
-
-    }
-
-    public void left(ActionEvent event){
-
-    }
-
-    public void right(ActionEvent event){
-
-    }
-
-//    public void back(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
-//        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
 
     @FXML
     private Button logoutButton;
@@ -66,67 +53,92 @@ public class PlayGameWindowController implements Initializable {
     @FXML
     private AnchorPane scenePane;
 
+    @FXML
+    private ImageView island1;
+
+    @FXML
+    private Label coordinate;
+
     public void back(ActionEvent event) throws IOException{
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Exit");
-//        alert.setHeaderText("Go to main menu");
         alert.setContentText("Do you want to go to main menu?");
-
-        if(alert.showAndWait().get() == ButtonType.OK){
-//            stage = (Stage) scenePane.getScene().getWindow();
-//            System.out.println("Game closed.");
-//            stage.close();
-            Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+        try {
+            if (alert.showAndWait().get() == ButtonType.OK) {
+                Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
         }
     }
-
+    @FXML
+    private Group textGroup;
+    @FXML
+    private TextField textField;
     @FXML
     private ImageView myImage;
-
     @FXML
     private ImageView myImage1;
+    @FXML
+    private ImageView orc;
+    @FXML
+    private ImageView island;
+    @FXML
+    private Group myGroup;
+    @FXML
+    private Group myGroup2;
+    @FXML
+    private Group myGroup3;
+    @FXML
+    private ImageView bossOrc;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1){
-
-        TranslateTransition translateTransition = new TranslateTransition();
-        TranslateTransition translateTransition1 = new TranslateTransition();
-        translateTransition.setNode(myImage);
-        translateTransition1.setNode(myImage1);
-        translateTransition.setDuration(Duration.millis(1000));
-        translateTransition1.setDuration(Duration.millis(1000));
-//        translateTransition.setByY(150);
-        translateTransition.setByY(-80);
-//        translateTransition.setFromY(-150);
-//        translateTransition1.setByY(150);
-        translateTransition1.setByY(-80);
-//        translateTransition1.setFromY(-150);
-        translateTransition.setCycleCount(TranslateTransition.INDEFINITE);
-        translateTransition1.setCycleCount(TranslateTransition.INDEFINITE);
-        translateTransition.setAutoReverse(true);
-        translateTransition1.setAutoReverse(true);
-        translateTransition.play();
-        translateTransition1.play();
+        position=0;
+        user=new User("",0);
+        takeInput();
+        Animate(myImage,Duration.millis(600),1,-60);
+        Animate(myImage1,Duration.millis(650),1,-70);
+        Animate(island,Duration.millis(2000),1,-15);
+        Animate(myGroup3,Duration.millis(2000),1,-15);
+        Animate(orc,Duration.millis(800),1,-50);
+        Animate(island1,Duration.millis(2000),1,-10);
+        Animate(bossOrc,Duration.millis(500),1,-30);
+    }
+    public void fadeAnimation(Node node,Duration duration,double from,double to){
+        FadeTransition fade=new FadeTransition(duration, node);
+        fade.setFromValue(from);
+        fade.setToValue(to);
+        fade.play();
+    }
+    public void takeInput(){
+        fadeAnimation(myGroup,Duration.millis(3000),1.0,0.36);
+        fadeAnimation(myGroup2,Duration.millis(3000),1.0,0.36);
+        Animate(textGroup,Duration.millis(50),0,175);
+    }
+    public void getUserName(){
+        user.setName(textField.getText());
+        Animate(textGroup,Duration.millis(650),0,-175);
+        fadeAnimation(myGroup,Duration.millis(3000),0.36,1.0);
+        fadeAnimation(myGroup2,Duration.millis(3000),0.36,1.0);
+        System.out.println(user.getName());
     }
 
-    public void pressSettingButton(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("settingPressPage.fxml"));
-//        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-
+    public void pressSettingButton() throws IOException {
         Stage stage = new Stage();
         AnchorPane anchorPane = new AnchorPane();
-
+        stage.setTitle("Settings");
+        stage.setHeight(200);
+        stage.setWidth(200);
         Button button1 = new Button("Save Game");
         button1.setPrefWidth(80);
-        button1.setTextAlignment(TextAlignment.CENTER);
-        button1.setLayoutX(55); button1.setLayoutY(50);
+        button1.setTextAlignment(TextAlignment.LEFT);
+        button1.setLayoutX(0); button1.setLayoutY(5);
         button1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -139,7 +151,7 @@ public class PlayGameWindowController implements Initializable {
 
         anchorPane.getChildren().addAll(button1, checkBox);
 
-        Image image = new Image("file:///D:/trial/1.0.jpg");
+        Image image = new Image("file:///C:/Users/shubh/IdeaProjects/Game/src/main/resources/com/example/game/Photos/1.0.png");
 
         Scene scene = new Scene(anchorPane);
         stage.setWidth(200);
@@ -151,6 +163,10 @@ public class PlayGameWindowController implements Initializable {
     }
 
     public void moveHero(ActionEvent event) throws IOException{
-
+        position+=25;
+        user.setScore(user.getScore()+1);
+        coordinate.setText(String.valueOf(user.getScore()));
+        myGroup.setTranslateX(-position);
+        myGroup2.setTranslateX(-position);
     }
 }
