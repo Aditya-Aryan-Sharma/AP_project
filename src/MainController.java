221331e6e6cd9e82546e713b3ec567d15577cd3b
Aditya Aryan Sharma <83038560+Aditya-Aryan-Sharma@users.noreply.github.com>
2021-12-22 19:38:32 +0530

    Update MainController.java

--- src/MainController.java
+++ src/MainController.java
@@ -1,16 +1,21 @@
 package com.example.game;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -20,12 +25,22 @@ public class MainController {
     private Scene scene;
     private Parent root;
 
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private AnchorPane scenePane;
+
     public void playGame(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("playGameWindow.fxml"));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("playGameWindow.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (NullPointerException e){
+            System.out.println("File Not found");
+        }
     }
 
     public void highScore(ActionEvent event) throws IOException{
@@ -37,29 +52,25 @@ public class MainController {
     }
 
     public void loadGame(ActionEvent event) throws IOException{
-        System.out.println("GAME LOADED");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("playGameWindow.fxml"));
+        root = loader.load();
+        PlayGameWindowController playGameWindowController=loader.getController();
+        playGameWindowController.setLoadedGame(true);
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
-
-    public void credits(ActionEvent event) throws IOException{
-        System.out.println("Developed by TUSHAR");
-    }
-
-    @FXML
-    private Button logoutButton;
-
-    @FXML
-    private AnchorPane scenePane;
 
     public void exit(ActionEvent event) throws IOException{
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Quit Game");
-//        alert.setHeaderText("Quit game");
         alert.setContentText("Do you want to quit game?");
-
         if(alert.showAndWait().get() == ButtonType.OK){
             stage = (Stage) scenePane.getScene().getWindow();
             System.out.println("Game closed.");
             stage.close();
         }
     }
+
 }
