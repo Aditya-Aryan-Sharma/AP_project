7d6bed4c60adea2bd9831cc0788099c3a649bce6
Aditya Aryan Sharma <83038560+Aditya-Aryan-Sharma@users.noreply.github.com>
2022-01-02 23:10:42 +0530

    Update HighScoreController.java

--- src/HighScoreController.java
+++ src/HighScoreController.java
@@ -13,50 +13,50 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class HighScoreController implements Initializable {
+public class HighScoreController implements Initializable,Serializable{
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-    private ArrayList<User> userList;
     @FXML
     private TableView<User> tableView;
+    @FXML
+    private TableColumn<User, String> name;
+    @FXML
+    private TableColumn<User, Integer> score;
+    private static ObservableList<User> board = FXCollections.observableArrayList(new User("Aryan",200), new User("Sharma",100)) ;
 
     public void back(ActionEvent event) throws IOException {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Problem Encountered While loading High score scene");
             e.printStackTrace();
         }
     }
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1){
-        tableView=new TableView<>();
-        userList=new ArrayList<>();
-        TableColumn<User,String> column1=new TableColumn<>("Player Name");
-        column1.setCellValueFactory(new PropertyValueFactory<>("name"));
-        TableColumn<User,String> column2=new TableColumn<>("Score");
-        column2.setCellValueFactory(new PropertyValueFactory<>("score"));
-        tableView.setItems(addEntry());
-        tableView.getColumns().addAll(column1,column2);
-
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        name.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
+        score.setCellValueFactory(new PropertyValueFactory<User, Integer>("score"));
+        tableView.setItems(board);
     }
-    public ObservableList<User> addEntry(){
-        userList.add(new User("Michael",5000));
-        ObservableList<User> users= FXCollections.observableList(userList);
-        return users;
+
+    public void addEntry(User user) {
+        board.add(user);
     }
 }
